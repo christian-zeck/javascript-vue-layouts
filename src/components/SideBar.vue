af3b4be84f99c8ed938e5869a05b20eede5256be
Christian Zeck <christian.zeck@gmail.com>
2018-11-04 07:09:40 +0200

    Added components

---
+++ src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sidebar">
+    <div class="box">
+      this is a sidebar box
+    </div>
+
+    <div class="box">
+      hey another side bar box
+    </div>
+
+    <div class="box">
+      yet another box
+    </div>
+  </div>
+</template>
